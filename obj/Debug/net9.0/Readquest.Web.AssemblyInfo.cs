a08455dd709aa84a5f9fc513feb69aa2b7f02776
Shugri7002 <Shugri7002@hotmail.com>
2025-10-31 05:34:37 +0100

    alles werkt behalve detailpage

--- obj/Debug/net9.0/Readquest.Web.AssemblyInfo.cs
+++ obj/Debug/net9.0/Readquest.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Readquest.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8c81a9b8547b1e5ffe06c0a92d61b8a5fd1c256")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa7f770bc3b8a5fea6d3dc8c7d856671466f11a0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Readquest.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Readquest.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
